99dfa3831b0ee3e7f5fc27a3af5fdec864fd5820
goodness <goodyc474@gmail.com>
2022-12-19 14:02:15 +0100

    add offering and tithes section

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const hambuger = document.querySelector(".hambuger");
 const mobileMenu = document.querySelector(".mobl-menu");
 const closeIcon = document.querySelector(".close-icon");
+const churchInfo = document.querySelectorAll(".church-info1");
 
 hambuger.addEventListener("click", () => {
   mobileMenu.classList.toggle("show");
@@ -8,54 +9,63 @@ hambuger.addEventListener("click", () => {
 closeIcon.addEventListener("click", () => {
   mobileMenu.classList.remove("show");
 });
-// let slider = document.getElementById("slider");
-let i = 1;
-setInterval(function () {
-  e = document.getElementById("slider");
-  console.log(e);
-  switch (i) {
-    case 0:
-      e.style.backgroundPosition = "center top, center top, center top";
-      break;
-    case 1:
-      e.style.backgroundPosition =
-        window.innerWidth + "px top, center top, center top";
-      break;
-    case 2:
-      e.style.backgroundPosition =
-        window.innerWidth +
-        "px top, " +
-        window.innerWidth +
-        "px top, center top";
-      break;
-  }
-  i++;
-  if (i > 2) i = 0;
-}, 3000);
-// function updateSlider() {
-//   switch (i) {
-//     case 0:
-//       slider.style.background = "center top, center top, center top";
-//       //       break;
-//       break;
-//     case 1:
-//       slider.style.backgroundPosition =
-//         window.innerWidth + "px top, center top, center top";
-//       break;
-//     case 2:
-//       slider.style.backgroundPosition =
-//         window.innerWidth +
-//         "px top, " +
-//         window.innerWidth +
-//         "px top, center top";
-//       break;
 
-//     default:
-//       break;
-//   }
-//   i++;
-//   if (i > 2) i = 0;
+const slider1 = document.querySelectorAll(".slider1");
+let position = 0;
+
+function showSlider() {
+  slider1.forEach((p) => {
+    p.style.display = "none";
+  });
+  position++;
+  if (position > slider1.length) {
+    position = 1;
+  }
+  slider1[position - 1].style.display = "block";
+}
+
+setInterval(() => {
+  showSlider();
+}, 1000);
+const observeChurchInfo = () => {
+  const observer = new IntersectionObserver(
+    (enteries) => {
+      enteries.forEach((e) => {
+        e.target.classList.toggle("show", e.isIntersecting);
+        e.isIntersecting
+          ? observer.unobserve(e.target)
+          : observer.observe(e.target);
+      });
+    },
+    {
+      threshold: 0,
+    }
+  );
+
+  churchInfo.forEach((info) => {
+    observer.observe(info);
+  });
+};
+observeChurchInfo();
+
+// function observeItems(observe) {
+//   const imgCont = document.querySelectorAll(".overall-img");
+//   const observer = new IntersectionObserver(
+//     (enteries) => {
+//       enteries.forEach((e) => {
+//         e.target.classList.toggle("show", e.isIntersecting);
+//         e.isIntersecting
+//           ? observer.unobserve(e.target)
+//           : observer.observe(e.target);
+//       });
+//     },
+//     {
+//       threshold: 1,
+//     }
+//   );
+
+//   imgCont.forEach((e) => {
+//     observer.observe(e);
+//   });
 // }
-// setInterval(() => {
-//   updateSlider();
-// }, 3000);
+// observeItems();
